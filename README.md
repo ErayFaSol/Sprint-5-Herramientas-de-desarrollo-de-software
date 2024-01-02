a45b89ef35071d19aaffae6e8a3f858cb673c563
Eduardo Franco <eduardo.fsolorzano@alumnos.udg.mx>
2024-01-02 15:20:03 -0600

    subir a render

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # proyecto5
+
+el objetivo de este proyecto es demostrar mi comprension de los temas del sprint 5: "Herramientas de desarrollo de software", mediante el uso de entornos virtuales de python y el despliegue de una aplicacion web en un servicio en la nube que lo haga accesible al publico 
+
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
+disp_button = st.button('Construir diagrama de dispersion')
 
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
@@ -14,3 +15,10 @@ if hist_button: # al hacer clic en el botón
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+if disp_button:
+    st.write('Creación de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
+
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    st.plotly_chart(fig, use_container_width=True )
